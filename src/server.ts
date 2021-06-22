cd4e8ca58df93c60d92b9545d49f11dade52d402
Cainã <caina@DESKTOP-2654VDS>
2021-06-22 08:03:46 -0300

    Create DataBase Sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from "express";
+import "reflect-metadata";
+import "./database";
+
 
 //@types/express
 const app = express();
+
+/**
+ * GET     => Buscar uma informação
+ * POST    => Inserir (Criar) uma informação
+ * PUT     => Alterar uma informação
+ * DELETE  => Remover um dado
+ * PATCH   => Alterar uma informação específica
+ */
+
+/**
+ * Tipos de parâmetros
+ * Routes Params => http://localhost:3000/produtos/55682312
+ * Query Params => http://localhost:3000/produtos?name=teclado&description=razer
+ */
 
 app.get("/test", (request, response) => {
 
@@ -13,7 +30,6 @@ app.post("/testPost", (resquest, response) => {
 
     return response.send("Teste Post")
 })
-
 
 
 //http://localhost:3000
